7ae5de7cda08e787b46494b2f7b047f118c58946
DrTipmack <ruzgar.altiner@gmail.com>
2022-02-23 21:26:17 +0000

    deployment

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import re
 client = discord.Client()
 
 tool = language_tool_python.LanguageTool("en-UK")
-
 query = r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"
 
 spamChannels = []
